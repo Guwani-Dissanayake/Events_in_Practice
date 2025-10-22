3c9094f169e0a469aec9d0ee7671e67d03f81c4c
IT21142628 <it21142628@my.sliit.lk>
2025-10-22 14:24:14 +0530

    web server listen to event that it emits

--- events.js
+++ events.js
@@ -1,4 +1,5 @@
 const EventEmitter = require('events');
+const http = require('http');
 
 class Sales extends EventEmitter{
     constructor(){
@@ -21,3 +22,27 @@ myEmitter.on("newSale", stock =>{
 });
 
 myEmitter.emit("newSale", 9);
+
+
+
+const server = http.createServer();
+
+server.on("request", (req, res) =>{
+    console.log("request received");
+    console.log(req.url);
+    res.end("request received");
+
+});
+
+server.on("request", (req, res) =>{
+    console.log("another request");
+
+});
+
+server.on("close", ()=>{
+    console.log("server closed");
+});
+
+server.listen(8000, "127.0.0.1", ()=>{
+    console.log("waiting for requests...");
+})
